e482f0b3f27eaef87575375157411c3ba651f751
knifofia <guillaume.crand2000@gmail.com>
2025-08-24 14:00:48 +0200

    fix build test1

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg';
   import Counter from './lib/Counter.svelte';
-  import viteLogo from '/vite.svg';
 </script>
 
 <main>
   <div>
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      <img src={"/vite.svg"} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
